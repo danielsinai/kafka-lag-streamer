fc3497525054ca8c74439308236ebab3172bde47
danielsinai <daniel.sinai.main@gmail.com>
2021-12-21 21:18:01 +0200

    added tests

--- test/config.js
+++ test/config.js
@@ -1,7 +1,7 @@
 const config = {
   brokers: ['localhost:9094'],
   topicToTestLagsOn: 'temp.topic',
-  group: 'groupy',
+  group: 'testing-group-not-commiting-offsets',
   groupMetadataTopic: 'group.metadata',
   consumerLagsTopic: 'consumer.lags',
 };
--- test/lags/index-spec.js
+++ test/lags/index-spec.js
@@ -1,24 +1,64 @@
 const config = require("../config");
 const assert = require("assert");
 const { Kafka } = require('kafkajs')
+import { v4 as uuidv4 } from 'uuid';
 
 const { brokers, topicToTestLagsOn, group, consumerLagsTopic, groupMetadataTopic } = config;
 
 describe("lags tests", function() {
-  describe("writing messages and reading and checking output topic ", function() {
-    it("should output the correct lag", async function() {
+  describe("live committing offset consumer group", function() {
+    it("should fetch every x seconds idle consumer group lag to topic", async function() {
+      const toProduce = uuidv4();
+      const producer = (new Kafka({ brokers })).producer();
+      const consumer1 = (new Kafka({ brokers })).consumer({ groupId: group });
+      const consumer2 = (new Kafka({ brokers })).consumer({ groupId: group });
+
+      try {
+        await consumer1.connect();
+        await consumer2.subscribe({ topic: consumerLagsTopic, fromBeginning: false})
+        const promise = new Promise((resolve, reject) => {
+          consumer2.run({ autoCommit: false, eachMessage: ({message}) => {
+              console.log(JSON.parse(message.toString()))
+            }
+          });
+        })
+        await producer.connect();
+        await producer.send({topic: topicToTestLagsOn, messages: [{key: null, value: toProduce}]});
+        await consumer1.subscribe({ topic: topicToTestLagsOn, fromBeginning: false})
+        consumer1.run({ eachMessage: ({resolveOffset, message}) => {
+            message.value.toString() === toProduce && resolveOffset()
+          }
+        }).then(() => console.log('running'));
+        await promise;
+        await consumer2.disconnect();
+        await consumer1.disconnect();
+        assert.ok(true);
+      } catch (e) {
+        console.log(e);
+        assert.ok(false);
+      }
+    });
+  });
+  describe("idle consumer groups", function() {
+    it("should fetch every x seconds idle consumer group lag to topic", async function() {
+      const toProduce = uuidv4();
       const producer = (new Kafka({ brokers })).producer();
       const consumer = (new Kafka({ brokers })).consumer({ groupId: group });
 
       try {
         await consumer.connect()
-        await consumer.subscribe({ topic: consumerLagsTopic, fromBeginning: true})
+        await consumer.subscribe({ topic: consumerLagsTopic, fromBeginning: false})
         const promise = new Promise((resolve, reject) => {
-          consumer.run({ autoCommit: false, eachMessage: ({message}) => console.log(message.value.toString())});
+          consumer.run({ autoCommit: false, eachMessage: ({message}) => {
+            setTimeout(reject, 20000)
+            JSON.parse((message.value).toString()).group === consumerLagsTopic && resolve();
+          }
+          });
         })
         await producer.connect();
-        await producer.send({topic: topicToTestLagsOn, messages: [{key: null, value: 'testing'}]});
+        await producer.send({topic: topicToTestLagsOn, messages: [{key: null, value: toProduce}]});
         await promise;
+        await consumer.disconnect();
         assert.ok(true);
       } catch (e) {
         console.log(e);
